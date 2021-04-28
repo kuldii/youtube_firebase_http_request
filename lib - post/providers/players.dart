6a802ee354e938ff49f74604b78fc70eec7da54e
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-04-28 16:53:16 +0700

    Bahan Ajar

---
+++ lib - post/providers/players.dart
@@ -0,0 +1,78 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../models/player.dart';
+
+class Players with ChangeNotifier {
+  List<Player> _allPlayer = [];
+
+  List<Player> get allPlayer => _allPlayer;
+
+  int get jumlahPlayer => _allPlayer.length;
+
+  Player selectById(String id) =>
+      _allPlayer.firstWhere((element) => element.id == id);
+
+  Future<void> addPlayer(String name, String position, String image) {
+    DateTime datetimeNow = DateTime.now();
+
+    Uri url = Uri.parse(
+        "https://http-req-bec2d-default-rtdb.firebaseio.com/players.json");
+    return http
+        .post(
+      url,
+      body: json.encode(
+        {
+          "name": name,
+          "position": position,
+          "imageUrl": image,
+          "createdAt": datetimeNow.toString(),
+        },
+      ),
+    )
+        .then(
+      (response) {
+        print("THEN FUNCTION");
+        _allPlayer.add(
+          Player(
+            id: json.decode(response.body)["name"].toString(),
+            name: name,
+            position: position,
+            imageUrl: image,
+            createdAt: datetimeNow,
+          ),
+        );
+
+        notifyListeners();
+      },
+    );
+  }
+
+  void editPlayer(String id, String name, String position, String image,
+      BuildContext context) {
+    Player selectPlayer = _allPlayer.firstWhere((element) => element.id == id);
+    selectPlayer.name = name;
+    selectPlayer.position = position;
+    selectPlayer.imageUrl = image;
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Berhasil diubah"),
+        duration: Duration(seconds: 2),
+      ),
+    );
+    notifyListeners();
+  }
+
+  void deletePlayer(String id, BuildContext context) {
+    _allPlayer.removeWhere((element) => element.id == id);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Berhasil dihapus"),
+        duration: Duration(milliseconds: 500),
+      ),
+    );
+    notifyListeners();
+  }
+}
