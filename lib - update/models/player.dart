eb04fac7d22b4864133d1c339562f7be21dd583d
Sandikha Rahardi <sandikha.rahardi@gmail.com>
2021-04-30 21:56:21 +0700

    Bahan Ajar

---
+++ lib - update/models/player.dart
@@ -0,0 +1,6 @@
+class Player {
+  String id, name, position, imageUrl;
+  DateTime createdAt;
+
+  Player({this.position, this.id, this.imageUrl, this.name, this.createdAt});
+}
---
+++ lib - update/providers/players.dart
@@ -0,0 +1,89 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import '../models/player.dart';
+
+class Players with ChangeNotifier {
+  List<Player> _allPlayer = [];
+
+  List<Player> get allPlayer => _allPlayer;
+
+  int get jumlahPlayer => _allPlayer.length;
+
+  Player selectById(String id) =>
+      _allPlayer.firstWhere((element) => element.id == id);
+
+  Future<void> addPlayer(String name, String position, String image) {
+    DateTime datetimeNow = DateTime.now();
+
+    Uri url = Uri.parse(
+        "https://http-req-bec2d-default-rtdb.firebaseio.com/players.json");
+    return http
+        .post(
+      url,
+      body: json.encode(
+        {
+          "name": name,
+          "position": position,
+          "imageUrl": image,
+          "createdAt": datetimeNow.toString(),
+        },
+      ),
+    )
+        .then(
+      (response) {
+        print("THEN FUNCTION");
+        _allPlayer.add(
+          Player(
+            id: json.decode(response.body)["name"].toString(),
+            name: name,
+            position: position,
+            imageUrl: image,
+            createdAt: datetimeNow,
+          ),
+        );
+
+        notifyListeners();
+      },
+    );
+  }
+
+  Future<void> editPlayer(
+      String id, String name, String position, String image) {
+    Uri url = Uri.parse(
+        "https://http-req-bec2d-default-rtdb.firebaseio.com/players/$id.json");
+    return http
+        .put(
+      url,
+      body: json.encode(
+        {
+          "name": name,
+          "position": position,
+          "imageUrl": image,
+        },
+      ),
+    )
+        .then(
+      (response) {
+        Player selectPlayer =
+            _allPlayer.firstWhere((element) => element.id == id);
+        selectPlayer.name = name;
+        selectPlayer.position = position;
+        selectPlayer.imageUrl = image;
+        notifyListeners();
+      },
+    );
+  }
+
+  Future<void> deletePlayer(String id) {
+    Uri url = Uri.parse(
+        "https://http-req-bec2d-default-rtdb.firebaseio.com/players/$id.json");
+    return http.delete(url).then(
+      (response) {
+        _allPlayer.removeWhere((element) => element.id == id);
+        notifyListeners();
+      },
+    );
+  }
+}
